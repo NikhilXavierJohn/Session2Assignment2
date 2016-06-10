a10ec275898e63401d83a92ec599268d0ae1cc62
NikhilXavierJohn <nkh.john@gmail.com>
2016-06-10 16:07:27 +0000

    Add files via upload

---
+++ Primenumbers.java
@@ -0,0 +1,25 @@
+
+public class Primenumbers {
+	 public static void main (String[] args)
+	   {		
+	       int i =0;
+	       int num =0;
+	       String  primeNumbers = "";
+
+	       for (i = 1; i <= 100; i++){ 	
+	    	  int count=0; 
+	          
+	          for(num =i; num>=1; num--){
+   	              if(i%num==0){
+	            	 count = count + 1;
+		     }
+		  }
+	          if (count == 2){			  
+		     primeNumbers = primeNumbers + i + " ";
+		  }	
+	   }	
+	       System.out.println("Prime numbers from 1 to 100 ");
+	       System.out.println(primeNumbers);
+	   }
+
+}
